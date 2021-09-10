fac20303140499b04804067ad07b32ab13386813
CMartin91 <90428869+CMartin91@users.noreply.github.com>
2021-09-09 20:48:50 -0400

    Add files via upload
    Practice for Thonny

---
+++ Demo thonny.py
@@ -0,0 +1,31 @@
+print(2+6)
+print(6-2)
+print(6*2)
+print(6/2)
+print(6%2)
+print(6**2)
+print(6+10*10+6)
+print((6+10)*(10+6))
+print(7+10-(2/2))
+x=10
+y=4
+z=x+y
+print(z)
+c="Hello"
+print(c)
+print(c[3])
+print(c[0:3])
+a=5
+b=3.2
+d="Hello"
+
+print("type of variable a is", type(a))
+print("type of variable a is", type(b))
+print("type of variable a is", type(c))
+points_obtained=88
+total_points=100
+percentage=88
+print(percentage/total_points)
+print((percentage/total_points)*100)
+percenatge=((points_obtained/total_points)*100)
+print("percentage are",percentage,"%")

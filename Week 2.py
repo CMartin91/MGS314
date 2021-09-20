d615b1fe4b69c903305690bcd62e2002a5ed7e78
CMartin91 <90428869+CMartin91@users.noreply.github.com>
2021-09-20 15:06:42 -0400

    Add files via upload
    Week 2 of recitation practice of replit

---
+++ Week 2.py
@@ -0,0 +1,78 @@
+'''
+my_string='This is my string'
+print("My string is", my_string)
+print( "String of my string is ", len (my_string))
+
+print("0 to 5 characters are " , my_string [0:5:2])
+'''
+
+'''
+my_string = "Jake"
+print("My name is", my_string)
+
+print("This is first string " + "This is second string")
+'''
+
+'''
+my_string = "I am a UB student"
+
+print( "My string in upper " , my_string.upper() )
+
+
+print( "My string in Lower is " , my_string.lower())
+
+print( "Splitted string is " , my_string.split(' '))
+
+print( "string is splitted by a ", my_string.split('a'))
+
+print( "Third string from split is " , my_string.split ('a') [2])
+'''
+
+'''
+print('Is 2 equal to 2?', 2==2 )
+print('Is 3 greater than 2?' , 3 >2)
+print('Is 3 less than 2?' , 3 <2 )
+'''
+'''
+students_list = ['Jake','bob','mike']
+
+print('List of students is ', students_list)
+'''
+
+'''
+student_ids = {
+  'Jake' : 1234,
+  'John' : 5678,
+  'Mike' : 4987
+}
+'''
+
+
+'''
+#Calc
+print("Enter the operation you want to perform")
+operation = input()
+
+a = 10
+b = 5
+
+if(operation == "sum"):
+  print(a + b)
+
+elif(operation == "sub"):
+  print(a - b)
+
+else:
+  print(a / b)
+'''
+
+#Calc
+
+hungry = True
+
+if hungry:
+  print('Feed Me')
+
+else: 
+  print("I'm not hungry")
+  
